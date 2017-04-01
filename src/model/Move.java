2f10f6b309befe34ad38e1b73b88330f49cfd0d1
Dragon Tran <dragon.tran@ou.edu>
2017-04-01 17:57:18 -0500

    Updates move class

--- src/model/Move.java
+++ src/model/Move.java
@@ -13,7 +13,6 @@ public class Move {
 		this.name = "boogy";
 		this.type = Element.Questionable;
 		this.baseDamage = 10;
-		
 	}
 	
 	//parameters constructor
@@ -24,5 +23,35 @@ public class Move {
 		this.baseDamage = baseDamage;
 	}
 	
+	public String getName(){
+		return name;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Element getType() {
+		return type;
+	}
+
+	public void setType(Element type) {
+		this.type = type;
+	}
+
+	public int getBaseDamage() {
+		return baseDamage;
+	}
+
+	public void setBaseDamage(int baseDamage) {
+		this.baseDamage = baseDamage;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
