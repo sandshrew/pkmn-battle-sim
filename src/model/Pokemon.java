399f97df66ba4ca119131810d5b5b32451915c1d
Dragon Tran <dragon.tran@ou.edu>
2017-04-01 17:57:29 -0500

    Updates pokemon class

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Pokemon {
 	//pokemon attributes/stats
 	private String name;
@@ -7,40 +10,93 @@ public class Pokemon {
 	private int baseHP;
 	private int baseAttack;
 	private int baseDefence;
-	
+	private int baseSpeed;
+	private Element type;
+	private List<Move> moves;
 	//maybe?
 	//private int baseSpecialAttack;
 	//private int baseSpecialDefence;
-	private int speed;
-	private Element type;
 	//attacks
 	//held item
-	
+
 	//maybe adding... EVs/IVs & nature
-	
-	
+
+	//battle stats
+	private int hp;
+	private int attack;
+	private int defence;
+	private int speed;
+
+
 	//default constructor
 	public Pokemon() {
 		//zoroark temp
 		this.name = "Zoroark";
 		this.level = 50;
-		this.baseHP = 120;
-		this.baseAttack = 110;
-		this.baseDefence = 65;
-		this.speed = 110;
+		this.baseHP = 60;
+		this.baseAttack = 105;
+		this.baseDefence = 60;
+		this.baseSpeed = 105;
 		this.type = Element.Dark;
-	}
 	
+		this.calculateHP();
+		this.attack = calculateStat(baseAttack);
+		this.defence = calculateStat(baseDefence);
+		this.speed = calculateStat(baseSpeed);
+		
+		this.moves = Arrays.asList(new Move(),new Move(),new Move(),new Move());
+	}
+
 	//constructor
-	public Pokemon(String name, int level, int baseHP, int baseAttack, int baseDefence, int speed, Element type) {
+	public Pokemon(String name, int level, int baseHP, int baseAttack, int baseDefence, int speed, Element type, List<Move> moves) {
 		this.name = name;
 		this.baseHP = baseHP;
 		this.baseAttack = baseAttack;
 		this.baseDefence = baseDefence;
-		this.speed = speed;
+		this.baseSpeed = speed;
 		this.type = type;
+		this.moves = moves;
+	}
+
+	//attack method that calculates damage output 
+	public int attack(Pokemon otherPokemon, Move attack){
+		return 10;
+	}
+
+	public void calculateHP(){
+		int IV = 0;
+		int EV = 0;
+		this.hp = (int) Math.floor(((2 * this.baseHP + IV + Math.floor(EV/4)) * this.level) / 100) + this.level + 10;
+	}
+
+	private int calculateStat(int base){
+		int nature = 1;
+		int IV = 0;
+		int EV = 0;
+		return (int) Math.floor((Math.floor(((2 * base + IV + EV) * this.level)/100) + 5) * nature);
 	}
 	
+	public void printStats(){
+		System.out.println("Name " + this.name);
+		System.out.println("HP " + this.hp);
+		System.out.println("Attack " + this.attack);
+		System.out.println("Defence " + this.defence);
+		System.out.println("Speed " + this.speed);
+		for(int i = 0; i < moves.size(); i++){
+			System.out.print(moves.get(i).getName() + " ");
+		}
+	}
+
+	//show moves
+	public void printMoves(){
+	}
+
+	//MAIN METHOD
+	public static void main(String[] args) {
+		Pokemon test = new Pokemon();
+		test.printStats();
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -88,11 +144,6 @@ public class Pokemon {
 	public void setType(Element type) {
 		this.type = type;
 	}
-	
-	public static void main(String[] args) {
-		Pokemon test = new Pokemon();
-		System.out.println(test.getName());
-	}
 
 	public int getLevel() {
 		return level;
@@ -101,7 +152,46 @@ public class Pokemon {
 	public void setLevel(int level) {
 		this.level = level;
 	}
-	
+
+	public List<Move> getMoves() {
+		return moves;
+	}
+
+	public void setMoves(List<Move> moves) {
+		this.moves = moves;
+	}
+
+	public int getBaseSpeed() {
+		return baseSpeed;
+	}
+
+	public void setBaseSpeed(int baseSpeed) {
+		this.baseSpeed = baseSpeed;
+	}
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getDefence() {
+		return defence;
+	}
+
+	public void setDefence(int defence) {
+		this.defence = defence;
+	}
 }
 
 
