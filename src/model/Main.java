b24daa1fb67c5f5c93c885f6bc172fd742b0d2a1
dragontran <dragon.tran@ou.edu>
2017-04-16 18:15:56 -0500

    Updates game engine
    Adds end game functionality, merges move/pokemon select, fixes priority, and removes some redundancies

--- src/model/Main.java
+++ src/model/Main.java
@@ -5,9 +5,11 @@ public class Main {
 	//MAIN METHOD
 	public static void main(String[] args) {
 		Pokemon test = new Pokemon();
-		test.printStats();
-		System.out.println();
-		//System.out.print(test.attack(test, test.getMoves().get(0)));
+		//test.printStats();
+		// for(int i = 0; i < 4; i++){
+		// System.out.print(test.getMoves().get(i).getName() + " ");
+		// }
+		// System.out.println();
 		
 		GameEngine ge = new GameEngine();
 		
