fcc2d86b015c41a60ebdfd13c36af970db82e3fb
dragontran <dragon.tran@ou.edu>
2017-04-25 00:03:01 -0500

    Adds parsing code and pokemon creation functionality

--- src/model/PokemonFactory.java
+++ src/model/PokemonFactory.java
@@ -7,35 +7,50 @@ import java.util.ArrayList;
 
 public class PokemonFactory {
 
-	public static void parseFile(){
-		try(BufferedReader reader = Files.newBufferedReader((Paths.get("PokemonData.csv")))){
+	public static void parseFile() {
+		try (BufferedReader reader = Files.newBufferedReader((Paths.get("PokemonData.csv")))) {
 			String line = reader.readLine();
-			while( line != null){
-				//System.out.println(line);
+			while (line != null) {
+				// System.out.println(line);
 				String[] tokens = line.split(",");
 				Pokemon pokemon = createPokemon(tokens);
 				line = reader.readLine();
 			}
-			
 
-		} catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public static Pokemon createPokemon(String[] tokens){
-		for(String s: tokens){
-			System.out.print(s + " ");
-		}
-		System.out.print('\n');
-		return null;
+
+	public static Pokemon createPokemon(String[] tokens) {
+		int tempID = Integer.parseInt(tokens[0]);
+		String tempName = tokens[1];
+		int tempHP = Integer.parseInt(tokens[2]);
+		int tempAtk = Integer.parseInt(tokens[3]);
+		int tempDef = Integer.parseInt(tokens[4]);
+		int tempSpeed = Integer.parseInt(tokens[5]);
+
+		Pokemon test = new Pokemon(tempID, tempName, 50, tempHP, tempAtk, tempDef, tempSpeed,
+				Element.valueOf(tokens[6]), createMove(tokens));
+		test.printStats();
+		return test;
 	}
-	
-	public static ArrayList<Move> createMove(String[] Moves){
-		return null;
+
+	public static ArrayList<Move> createMove(String[] tokens) {
+		ArrayList<Move> temp = new ArrayList<Move>();
+		int next = 0;
+		for (int i = 0; i < 4; i++) {
+			Move move = new Move(tokens[7 + next], Element.valueOf(tokens[9 + next]),
+					Integer.parseInt(tokens[8 + next]));
+			temp.add(move);
+			next += 3;
+		}
+		return temp;
 	}
 
 	public static void main(String[] args) {
 		parseFile();
+//		Pokemon test = new Pokemon();
+//		test.printStats();
 	}
 }
