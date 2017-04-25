d0ff1bf7813091935f298959ce7a7d2336f1530b
dragontran <dragon.tran@ou.edu>
2017-04-24 23:17:00 -0500

    Removes unncessary attributes

--- src/model/Move.java
+++ src/model/Move.java
@@ -2,45 +2,28 @@ package model;
 
 public class Move {
 	
-	private int id;
 	private String name;
 	private Element type;
 	private int baseDamage;
-	private boolean physical;
 	
 	//default constructor
 	public Move(){
-		this.id = -1;
 		this.name = "U-Turn";
 		this.type = Element.Bug;
 		this.baseDamage = 70;
-		physical = true;
 	}
 	
 	//parameters constructor
-	public Move(int id, String name, Element type, int baseDamage, boolean phys) {
-		this.id = id;
+	public Move(String name, Element type, int baseDamage) {
 		this.name = name;
 		this.type = type;
 		this.baseDamage = baseDamage;
-		this.physical = phys;
-	}
-	
-	public boolean isPhysical(){
-		return this.physical;
 	}
 	
 	public String getName(){
 		return name;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
 
 	public Element getType() {
 		return type;
