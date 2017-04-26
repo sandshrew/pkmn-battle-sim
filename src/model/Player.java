aae895dc1972503352451a66ba780b66e303bba5
dragontran <dragon.tran@ou.edu>
2017-04-26 13:05:10 -0500

    Removes some redundant code in player model

--- src/model/Player.java
+++ src/model/Player.java
@@ -4,30 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Player 
-{
-	
+public class Player {
+
 	private String playerName;
 	private String playerId;
 	private ArrayList<Pokemon> pokeParty;
 	private static final int MAX_POKEMON_COUNT = 6;
-	private boolean moveSelected;
-	
-	public Player()
-	{
-	
+	//private boolean moveSelected;
+
+	public Player() {
+
 		this.setPlayerName("Red");
 		this.setPlayerId("red123");
-		this.pokeParty = new ArrayList<Pokemon>(Arrays.asList(new Pokemon(), new Pokemon(), new Pokemon(), new Pokemon(), new Pokemon(), new Pokemon()));
-		this.setMoveSelected(false);
-	
+		this.pokeParty = new ArrayList<Pokemon>(Arrays.asList(new Pokemon(), new Pokemon(), new Pokemon(),
+				new Pokemon(), new Pokemon(), new Pokemon()));
+		//this.setMoveSelected(false);
+
 	}
-	
-	public ArrayList<Pokemon> getPokeParty(){
+
+	public Player(String name, ArrayList<Pokemon> test) {
+		this.playerName = name;
+		this.pokeParty = test;
+
+		this.playerId = "1";
+		//this.moveSelected = false;
+	}
+
+	public ArrayList<Pokemon> getPokeParty() {
 		return pokeParty;
 	}
-	
-	public void setPokeParty(ArrayList<Pokemon> pokeParty){
+
+	public void setPokeParty(ArrayList<Pokemon> pokeParty) {
 		this.pokeParty = pokeParty;
 	}
 
@@ -51,12 +58,12 @@ public class Player
 		return MAX_POKEMON_COUNT;
 	}
 
-	public boolean isMoveSelected() {
-		return moveSelected;
-	}
+//	public boolean isMoveSelected() {
+//		return moveSelected;
+//	}
+//
+//	public void setMoveSelected(boolean moveSelected) {
+//		this.moveSelected = moveSelected;
+//	}
 
-	public void setMoveSelected(boolean moveSelected) {
-		this.moveSelected = moveSelected;
-	}
-	
 }
